c7f0e78c948d45a0101f3e90600d0df6908876b2
Steve Miles <steven.miles.quant@gmail.com>
2024-02-14 19:42:03 -0500

    added custom response

--- app/src/components/ChoicesCards.tsx
+++ app/src/components/ChoicesCards.tsx
@@ -5,6 +5,9 @@ import {
   Choice,
 } from "../hooks/useAdventure";
 import CardContainer from "./CardContainer";
+import { Box, IconButton, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
 
 interface Props {
   playerName: string;
@@ -18,21 +21,48 @@ interface Props {
 }
 
 const ChoicesCards = ({ playerName, choices, addInteraction }: Props) => {
+  const [customResponse, setCustomResponse] = useState("");
+
   return (
     <>
       {choices.map((choice) => (
         <CardContainer
-          onClick={() => {
+          onClick={() =>
             addInteraction.mutate({
               player_name: playerName,
               choice: choice,
-            } as Choice);
-          }}
+            } as Choice)
+          }
           key={choice}
         >
           {choice}
         </CardContainer>
       ))}
+      <Box>
+        <Input
+          placeholder="Custom response..."
+          onChange={(event) => setCustomResponse(event.target.value)}
+        />
+        <IconButton
+          icon={<FaArrowRightFromBracket />}
+          aria-label="Submit custom response"
+          position="absolute"
+          right={5}
+          marginRight={3}
+          bgColor="white"
+          borderColor="gray.200"
+          borderStyle="solid"
+          borderWidth={1}
+          borderRadius={6}
+          zIndex={1}
+          onClick={() =>
+            addInteraction.mutate({
+              player_name: playerName,
+              choice: customResponse,
+            } as Choice)
+          }
+        />
+      </Box>
     </>
   );
 };
